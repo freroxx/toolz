1edadee13dea9f411a3afb5de29c47889975f33f
freroxx <yikni2829@example.com>
2026-03-23 13:53:18 +0100

    New app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.frerox.toolz"
         minSdk = 31
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
