30ff98ce4c71c2902968b3010544169c82a49765
freroxx <yikni2829@example.com>
2026-05-04 16:06:47 +0100

    feat(pdf): make DocumentHandler implement ConversionHandler

--- app/src/main/java/com/frerox/toolz/data/pdf/DocumentHandler.kt
+++ app/src/main/java/com/frerox/toolz/data/pdf/DocumentHandler.kt
@@ -2,7 +2,8 @@ package com.frerox.toolz.data.pdf
 
 import android.net.Uri
 import android.text.Spanned
+import com.frerox.toolz.util.converters.ConversionHandler
 
-interface DocumentHandler {
+interface DocumentHandler : ConversionHandler {
     suspend fun processDocuments(inputUris: List<Uri>, usableHeight: Int): List<Spanned>
 }
