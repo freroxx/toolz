02df47a995216d4df380ec80f274ab19ba5318dd
FREROX <yikni2829@gmail.com>
2026-07-16 21:56:54 +0100

    v1.0.9 (REMOVED MUSIC WIDGET INTENT ACTIONS)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.frerox.toolz"
         minSdk = 31
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
