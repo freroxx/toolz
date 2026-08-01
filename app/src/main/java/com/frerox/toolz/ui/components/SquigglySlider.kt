21749d703bbae2a9f38b3a33a625d803fee1d68f
freroxx <yikni2829@gmail.com>
2026-08-01 14:32:44 +0100

    v1.0.9 (ADDED EVEN MORE LICENSE HEADERS)

--- app/src/main/java/com/frerox/toolz/ui/components/SquigglySlider.kt
+++ app/src/main/java/com/frerox/toolz/ui/components/SquigglySlider.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2026 Toolz Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.frerox.toolz.ui.components
 
 import androidx.compose.animation.core.*
@@ -50,6 +67,7 @@ fun SquigglySlider(
     value: () -> Float,
     onValueChange: (Float) -> Unit,
     modifier: Modifier = Modifier,
+    enabled: Boolean = true, // Added for compatibility if needed, but not in original. Wait, the original didn't have enabled.
     onValueChangeFinished: () -> Unit = {},
     valueRange: ClosedFloatingPointRange<Float>,
     isPlaying: Boolean = true,
